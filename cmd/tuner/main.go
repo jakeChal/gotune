90d040ef49e8bfd49b56b02122b80df5b5a2460b
jakeChal <jakeChal@users.noreply.github.com>
2025-12-07 18:12:38 +0100

    Full demo

--- cmd/tuner/main.go
+++ cmd/tuner/main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"gitlab.com/jacobidis/gotune/pkg/audio"
 	"gitlab.com/jacobidis/gotune/pkg/dsp"
 )
 
+const (
+	sampleRate = 48000
+	bufferSize = 4096
+
+	// Guitar specific
+	minFreq = 75.0   // Slightly below E2 (82.4 Hz)
+	maxFreq = 1400.0 // Slightly above E6 (1318 Hz)
+)
+
 func main() {
-	const sampleRate = 48000
+	threshold := flag.Float64("t", 0.1, "The MPM algorithm's detection threshold [0 - 1.0]. Low values can lead to false positives")
+	flag.Parse()
+	if *threshold < 0 || *threshold > 1.0 {
+		fmt.Fprintf(os.Stderr, "threshold: %v is out of range!\n", *threshold)
+		os.Exit(1)
+	}
 
 	ai, err := audio.NewAudioInput(sampleRate)
 	if err != nil {
@@ -22,8 +38,25 @@ func main() {
 
 	fmt.Println("Listening...")
 
+	bufferAccum := make([]float64, 0, bufferSize)
+
 	for frame := range ai.Frames {
-		val := dsp.PassThrough(frame) // later replace with pitch estimate
-		fmt.Printf("\rEnergy: %.2f      ", val)
+		frame64 := dsp.Float32ToFloat64(frame)
+		bufferAccum = append(bufferAccum, frame64...)
+
+		if len(bufferAccum) >= bufferSize {
+			result := dsp.DetectPitch(bufferAccum[:bufferSize], sampleRate, *threshold)
+
+			// display
+			if result.HasPitch && result.Frequency >= minFreq && result.Frequency <= maxFreq {
+				noteName, _, centsOff := dsp.PitchToNote(result.Frequency)
+				fmt.Printf(
+					"\rFrequency detected: %9.2f | %-4s %4d cents [confidence: %5.2f]",
+					result.Frequency, noteName, int(centsOff), result.Clarity,
+				)
+			}
+			// reset
+			bufferAccum = bufferAccum[:0]
+		}
 	}
 }
---
+++ pkg/dsp/helpers.go
@@ -0,0 +1,10 @@
+package dsp
+
+func Float32ToFloat64(in []float32) []float64 {
+	out := make([]float64, len(in))
+	for i, v := range in {
+		out[i] = float64(v)
+	}
+
+	return out
+}
